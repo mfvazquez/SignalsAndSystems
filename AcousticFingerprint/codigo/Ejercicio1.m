22f6ec23d305291bc5711077221904fa07df522f
mfvazquez <mfvazquez@fi.uba.ar>
2015-11-11 02:28:00 -0300

    agregado el ejercicio 1

---
+++ AcousticFingerprint/codigo/Ejercicio1.m
@@ -0,0 +1,14 @@
+info = audioinfo("caprici_di_diablo.wav");
+audio_doble = audioread("caprici_di_diablo.wav");
+
+audio = mean(audio_doble'(:,:));
+ 
+intervalo = info.Duration/info.TotalSamples;
+figure 1
+plot(0:intervalo:249*intervalo ,audio(1:250));
+xlabel ("tiempo [s]");
+ylabel ("Canal de audio");
+print('canal.png','-dpng');
+
+player = audioplayer(audio, info.SampleRate);
+play(player);

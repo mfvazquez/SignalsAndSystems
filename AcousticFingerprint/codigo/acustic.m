471ba2c7929fdaf2d316ca77f1e9935252513cf5
Matias Vazquez <mfvazquez@fi.uba.ar>
2015-11-16 18:17:54 -0300

    empezado el ej3

--- AcousticFingerprint/codigo/acustic.m
+++ AcousticFingerprint/codigo/acustic.m
@@ -1,3 +1,4 @@
+pkg load signal
 % -------------------------- Ejercicio 1 ---------------------------------------
 
 info = audioinfo("audios/caprici_di_diablo.wav");
@@ -7,17 +8,20 @@ audio = mean(audio_doble,2);
  
 intervalo = info.Duration/info.TotalSamples;
 figure 1
-plot(0:intervalo:249*intervalo ,audio(1:250));
+N = 265;
+plot(0:intervalo:(N-1)*intervalo ,audio(1:N));
 xlabel ("tiempo [s]");
 ylabel ("Canal de audio");
-print('canal.png','-dpng');
+print('canal_cdd.png','-dpng');
 
 %player = audioplayer(audio, info.SampleRate);
 %play(player);
 
 % -------------------------- Ejercicio 2 ---------------------------------------
-N = 5000;
-[S, f, t] = specgram(audio, N, info.SampleRate);
+
+N = 512;
+window(1:N-1) = 1;
+[S, f, t] = specgram(audio, N, info.SampleRate, N, 0);
 S = 10*log10(abs(S).^2);
 S = mean(S',length(f(:,1)));
 
@@ -25,11 +29,10 @@ fft_S = fft(audio);
 fft_S = 10*log10(abs(fft_S).^2);
 
 figure 2
-plot(f(1,:),S(1,:));
+plot([-f(1,end:-1:1),f(1,:)],[S(end:-1:1),S(1,:)]);
 
 xlabel("frecuencia [Hz]");
 ylabel("Densidad de potencia");
-print('densidad_frecuencias.png','-dpng');
+print('densidad_frecuencias_cdd.png','-dpng');
 
-figure 3
-plot(fft_S);
+% -------------------------- Ejercicio 3 ---------------------------------------
